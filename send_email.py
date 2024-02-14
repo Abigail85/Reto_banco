165366b07b3b8d8c1eb4ba7d5010742be617f7d8
Jhon Edinson Acevedo <jhon.acevedo@quipux.com>
2024-02-14 13:47:19 +0000

    prints

--- send_email.py
+++ send_email.py
@@ -12,6 +12,14 @@ def send_email(subject, message):
     smtp_username = os.getenv("SMTP_USERNAME")
     smtp_password = os.getenv("SMTP_PASSWORD")
 
+    # Print variables
+    print("Sender Email:", sender_email)
+    print("Receiver Email:", receiver_email)
+    print("SMTP Server:", smtp_server)
+    print("SMTP Port:", smtp_port)
+    print("SMTP Username:", smtp_username)
+    print("SMTP Password:", smtp_password)
+
     # Create message container.
     msg = MIMEMultipart()
     msg['Subject'] = subject
@@ -22,10 +30,15 @@ def send_email(subject, message):
     part = MIMEText(message, 'plain')
     msg.attach(part)
 
-    # Send the message via SMTP server.
-    with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
-        server.login(smtp_username, smtp_password)
-        server.sendmail(sender_email, receiver_email, msg.as_string())
+    try:
+        # Send the message via SMTP server.
+        with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+            server.login(smtp_username, smtp_password)
+            server.sendmail(sender_email, receiver_email, msg.as_string())
+        print("Correo enviado correctamente.")
+    except Exception as e:
+        print("Error al enviar correo:", str(e))
+
 
 if __name__ == "__main__":
     # Solo se ejecuta si se importa como módulo, no cuando se llama directamente desde la línea de comandos
