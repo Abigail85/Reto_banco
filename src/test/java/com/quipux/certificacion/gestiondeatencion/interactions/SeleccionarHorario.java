b42ab7c27fecc91c2e10612c58c2eca7aa372b4c
Julian Rua <jruav@devco.com.co>
2024-02-28 14:37:01 -0500

    Cambio en el mapeo de seleccionar el horario

--- src/test/java/com/quipux/certificacion/gestiondeatencion/interactions/SeleccionarHorario.java
+++ src/test/java/com/quipux/certificacion/gestiondeatencion/interactions/SeleccionarHorario.java
@@ -1,15 +1,12 @@
 package com.quipux.certificacion.gestiondeatencion.interactions;
 
-import net.serenitybdd.annotations.Step;
 import net.serenitybdd.model.time.InternalSystemClock;
-import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
-import net.serenitybdd.screenplay.actions.Enter;
 
-import static com.quipux.certificacion.gestiondeatencion.userinterface.AgendarCitaPage.*;
-import static net.serenitybdd.screenplay.Tasks.instrumented;
+import static com.quipux.certificacion.gestiondeatencion.userinterface.AgendarCitaPage.LST_HORARIO_DISPONIBLE;
+import static com.quipux.certificacion.gestiondeatencion.userinterface.AgendarCitaPage.LST_HORA_SELECCIONADA;
 
 public class SeleccionarHorario {
 
@@ -19,7 +16,6 @@ public class SeleccionarHorario {
                     new InternalSystemClock().pauseFor(3000);
                     actor.attemptsTo(
                             Click.on(LST_HORARIO_DISPONIBLE),
-                            Enter.theValue("19").into(TXT_INGRESAR_HORA_DESEADA),
                             Click.on(LST_HORA_SELECCIONADA));
                 });
     }
